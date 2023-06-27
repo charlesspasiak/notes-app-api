71ffb5c8a42cef15817c0c506aeebfc08ab809ef
Charles Pasiak <charlespasiak@gmail.com>
2023-06-27 13:16:27 +0700

    fix bug in UsersService.js

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -14,13 +14,15 @@ class UsersService {
     await this.verifyNewUsername(username);
 
     const id = `user-${nanoid(16)}`;
-    const hashedPassword = bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
       values: [id, username, hashedPassword, fullname],
     };
 
     const result = await this._pool.query(query);
+    // console.log(result);
 
     if (!result.rows.length) {
       throw new InvariantError('User gagal ditambahkan');
